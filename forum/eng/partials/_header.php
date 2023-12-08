2f9a90bdb94109ab20254afe205f7f6cc48e719b
netkoizmase1 <139656313+netkoizmase1@users.noreply.github.com>
2023-12-08 01:59:30 +0100

    Update _header.php

--- forum/eng/partials/_header.php
+++ forum/eng/partials/_header.php
@@ -22,7 +22,7 @@
 session_start();
 
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<a class="navbar-brand" href="/forum/indexeng.php">OpenSesame</a>
+<a class="navbar-brand" href="/forum/eng/indexeng.php">OpenSesame</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
@@ -30,7 +30,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href="/forum/indexeng.php">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="/forum/eng/indexeng.php">Home <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="about.php">About</a>
